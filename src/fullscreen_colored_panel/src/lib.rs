c566065fb5cd7adcec306ff5bcdf1621fdbe7f59
tommywalkie <tommywalkie@gmail.com>
2020-04-16 12:24:46 +0200

    Finding and uploading artifact for windows

--- src/fullscreen_colored_panel/src/lib.rs
+++ src/fullscreen_colored_panel/src/lib.rs
@@ -18,14 +18,14 @@ pub unsafe fn generate_fullscreen_blue_panel(owner: Node) -> Panel {
     let mut background: Panel = generate_fullscreen_panel(owner);
     let (godot_string, stylebox) = generate_style(Color::rgba(0.576471, 0.313726, 0.92549, 1.0));
     background.add_stylebox_override(godot_string, stylebox);
-    (background)
+    background
 }
 
 pub unsafe fn generate_fullscreen_green_panel(owner: Node) -> Panel {
     let mut background: Panel = generate_fullscreen_panel(owner);
     let (godot_string, stylebox) = generate_style(Color::rgba(0.529412, 0.937255, 0.447059, 1.0));
     background.add_stylebox_override(godot_string, stylebox);
-    (background)
+    background
 }
 
 unsafe fn generate_style(color: Color) -> (GodotString, Option<StyleBox>) {
@@ -41,5 +41,5 @@ unsafe fn generate_fullscreen_panel(owner: Node) -> Panel {
     let mut background = Panel::new();
     background.set_margin(2, size.x.into());
     background.set_margin(3, size.y.into());
-    (background)
+    background
 }
